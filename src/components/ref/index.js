7d51bd8e283da755adce5260495c825cbabf104b
Collins <mutaic84@gmail.com>
2022-05-20 11:18:31 +0300

    complete code for useRef hook use cases

--- src/components/ref/index.js
+++ src/components/ref/index.js
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 function App() {
+  const [name, setName] = useState("")
+
+  const renderCount = useRef(0)
+  const inputRef = useRef()
+  const prevName = useRef('')
+
+  useEffect(() => {
+    renderCount.current = renderCount.current + 1
+  })
+
+  function focus() {
+    inputRef.current.focus()
+  }
+
+  // store previous value of state
+  useEffect(() => {
+    prevName.current = name
+  }, [name])
   return (
     <>
       <hr />
       <h2>UseRef Hook</h2>
+      <input ref={inputRef} value={name} onChange={(e) => setName(e.target.value)}/>
+      <div>My name is {name}</div>
+      <div>My name used to be {prevName.current}</div>
+      <div>I rendered {renderCount.current} times</div>
+
+      {/* create a button to change focus to input field when clicked */}
+      <button onClick={focus}>Focus</button>
     </>
   )
 }

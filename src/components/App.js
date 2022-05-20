2147c26a88f6449f3ceec368169948e41e359c71
Collins <mutaic84@gmail.com>
2022-05-20 10:53:02 +0300

    display all components on page

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import State from "./state"
 import Effect from "./effect"
+import Memo from "./memo"
 
 function App() {
   return (
     <>
       <State />
       <Effect />
+      <Memo />
     </>
   )
 }

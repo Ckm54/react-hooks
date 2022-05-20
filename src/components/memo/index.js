0efe9c52b0faf3cf49fe9e924962ce471bab3eb2
Collins <mutaic84@gmail.com>
2022-05-20 10:11:46 +0300

    code out basic functionality of the useMemo hook application

--- src/components/memo/index.js
+++ src/components/memo/index.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function App() {
+  const [number, setNumber] = useState(0)
+  const [dark, setDark] = useState(false)
+  const doubleNumber = slowFunction(number)
+  const themeStyles = {
+    backgroundColor: dark ? "black" : "white",
+    color: dark ? "white" : "black"
+  }
+
   return (
-    <div>App</div>
+    <>
+      <input type="number" value={number} onChange={(e) => {
+        setNumber(parseInt(e.target.value))
+      }} />
+      <button onClick={() => setDark(prevDark => !prevDark)}>Change Theme</button>
+      <div style={themeStyles}>{ doubleNumber }</div>
+    </>
   )
+}
+
+
+// some code to simulate the process of a slow application process
+function slowFunction(num) {
+  console.log("calling slow function")
+  for (let i = 0; i <= 1000000000; i++) {}
+  return num * 2
 }
 
 export default App

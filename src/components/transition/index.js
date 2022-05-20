9af02f2f23ac53adc1fb79f2a6a7fec1105d0d3f
Collins <mutaic84@gmail.com>
2022-05-20 17:26:17 +0300

    complete code for the useTransition hook

--- src/components/transition/index.js
+++ src/components/transition/index.js
@@ -1,24 +1,32 @@
-import React, { useState } from 'react'
+import React, { useState, useTransition } from 'react'
 
 // emulate an application with alot of data in it
 function App() {
+
+  const [isPending, startTransition] = useTransition()
+
   const [input, setInput] = useState('')
   const [list, setList] = useState([])
 
   const LIST_SIZE = 20000;
 
   function handleChange(e) {
+    // this code is high priority
     setInput(e.target.value)
-    const l = []
-    for (let i = 0; i < LIST_SIZE; i++) {
-      l.push(e.target.value)
-    }
-    setList(l)
+    // this operation takes long and slows down the application...has some lower priority
+    // this code is low priority
+    startTransition(() => {
+      const l = []
+      for (let i = 0; i < LIST_SIZE; i++) {
+        l.push(e.target.value)
+      }
+      setList(l)
+    })
   }
   return (
     <>
       <input type="text" value={input} onChange={handleChange} />
-      {list.map((item, index) => {
+      {isPending ? "Loading..." : list.map((item, index) => {
         return <div key={index}>{item}</div>
       })}
     </>

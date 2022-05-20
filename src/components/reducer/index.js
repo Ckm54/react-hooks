35ea3892622fbef6e42236cc951ed0ce847cd194
Collins <mutaic84@gmail.com>
2022-05-20 14:01:37 +0300

    complete code for useReducer hook

--- src/components/reducer/index.js
+++ src/components/reducer/index.js
@@ -1,40 +1,55 @@
-import React, { useReducer } from 'react'
+import React, { useState, useReducer } from 'react'
+import { act } from 'react-dom/test-utils'
+import Todo from './Todo'
 
-const ACTIONS = {
-  INCREMENT: 'increment',
-  DECREMENT: 'decrement',
+export const ACTIONS = {
+  NEW_TODO: 'add-todo',
+  TOGGLE_TODO: 'toggle-todo',
+  DELETE_TODO: 'delete-todo',
 }
 
-const reducer = (state, action) => {
-  switch(action.type){
-    case ACTIONS.INCREMENT:
-      return {count: state.count + 1}
-    case ACTIONS.DECREMENT:
-      return {count: state.count - 1}
-    default:
-      return state
-  }
+const reducer = (todos, action) => {
   
+  switch(action.type) {
+    case ACTIONS.NEW_TODO:
+      return [...todos, newTodo(action.payload.name)]
+      
+    case ACTIONS.TOGGLE_TODO:
+      return todos.map(todo => {
+        if(todo.id === action.payload.id){
+          return { ...todo, complete: !todo.complete}
+        }
+        return todo
+      })
+    case ACTIONS.DELETE_TODO:
+      return todos.filter((todo) => todo.id !== action.payload.id)
+    default:
+      return true
+  }
+}
+
+
+function newTodo(name) {
+  return {id: Date.now(), name: name, complete: false}
 }
 function App() {
+  const [todos, dispatch] = useReducer(reducer, [])
+  const [name, setName] = useState('')
 
-  const [state, dispatch] = useReducer(reducer, {count: 0})
-  // const [count, setCount] = useState(0)
-
-  function incrementCount() {
-    dispatch({type: ACTIONS.INCREMENT})
+  function handleSubmit(e){
+    e.preventDefault()
+    dispatch({type: ACTIONS.NEW_TODO, payload: {name: name}})
+    setName('')
   }
 
-  function decrementCount() {
-    dispatch({type: ACTIONS.DECREMENT})
-  }
   return (
-    <div>
-      <h2>Using the useReducer hook</h2>
-      <button onClick={decrementCount}>-</button>
-      <span>{state.count}</span>
-      <button onClick={incrementCount}>+</button>
-    </div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+      </form>
+      
+      {todos ? todos.map((todo) => <Todo key={todo.id} todo={todo} dispatch={dispatch} />) : null}
+    </>
   )
 }
 

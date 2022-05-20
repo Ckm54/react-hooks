ea022185071d4a06b15c088c5f97cc27e2a57b2a
Collins <mutaic84@gmail.com>
2022-05-20 09:42:50 +0300

    complete code example to demonstrate useEffect hook usage

--- src/components/effect/index.js
+++ src/components/effect/index.js
@@ -1,8 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 function App() {
+  // useEffect hook is used when there is going to be some side effect happening within code e.g fetching data from an Api
+  const [resourceType, setResourceType] = useState('posts')
+  const [items, setItems] = useState([])
+
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+    .then(response => response.json())
+    .then(json => setItems(json))
+  }, [resourceType])
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth)
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize)
+
+    // do a cleanup so as not to constantly add an event listener when the component unmounts
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
   return (
-    <div>App</div>
+    <>
+      <hr />
+      <h2>Use Effect Hook</h2>
+      <hr />
+      <h2>useEffect with window width</h2>
+      <div>Window width: {windowWidth}</div>
+
+      <hr />
+      <div>
+        <button onClick={() => setResourceType('posts')}>Posts</button>
+        <button onClick={() => setResourceType('users')}>Users</button>
+        <button onClick={() => setResourceType('comments')}>Comments</button>
+      </div>
+      <h1>{ resourceType }</h1>
+      {items.map((item) => {
+        return <pre key={item.id}>{JSON.stringify(item)}</pre>
+      })}
+    </>
   )
 }
 

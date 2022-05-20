e08bb4dbdd9fbfb0273c791ae388476e8093094f
Collins <mutaic84@gmail.com>
2022-05-20 16:32:25 +0300

    complete code for useCallback react hook

--- src/components/callback/index.js
+++ src/components/callback/index.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
+import List from "./List"
 
 function App() {
+  const [number, setNumber] = useState(1)
+  const [dark, setDark] = useState(false)
+
+  // only recreate this when number changes
+  const getItems = useCallback(() => {
+    return [number, number+1, number+2]
+  }, [number])
+
+  const theme = {
+    backgroundColor: dark ? "#333" : "#fff",
+    color: dark ? "#fff" : "#333"
+  }
   return (
-    <div>App</div>
+    <div style={theme}>
+      <input type="number" value={number} onChange={(e) => setNumber(parseInt(e.target.value))} />
+      <button onClick={() => setDark(prevDark => !prevDark)}>Toggle Theme</button>
+      <List getItems={getItems} />
+    </div>
   )
 }
 

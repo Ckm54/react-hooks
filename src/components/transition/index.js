2377670698148f5ada59ad9a523a79700e2d15ed
Collins <mutaic84@gmail.com>
2022-05-20 17:17:52 +0300

    create a component to practice using the useTransition react hook

---
+++ src/components/transition/index.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+
+// emulate an application with alot of data in it
+function App() {
+  const [input, setInput] = useState('')
+  const [list, setList] = useState([])
+
+  const LIST_SIZE = 20000;
+
+  function handleChange(e) {
+    setInput(e.target.value)
+    const l = []
+    for (let i = 0; i < LIST_SIZE; i++) {
+      l.push(e.target.value)
+    }
+    setList(l)
+  }
+  return (
+    <>
+      <input type="text" value={input} onChange={handleChange} />
+      {list.map((item, index) => {
+        return <div key={index}>{item}</div>
+      })}
+    </>
+  )
+}
+
+export default App
